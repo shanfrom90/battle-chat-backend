5ef621b23e5660eb316846298749429de40ba5ef
Shannon Adams <77690999+shanfrom90@users.noreply.github.com>
2024-02-13 15:45:34 -0500

    initial commit, excluded datasource for now

---
+++ src/main/java/com/battle/chat/demo/BattleChatBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.battle.chat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleChatBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleChatBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/battle/chat/demo/BattleChatBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.battle.chat.demo;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class BattleChatBackEndApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
