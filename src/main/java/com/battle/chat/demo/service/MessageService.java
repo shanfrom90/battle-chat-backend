326ab7ccf8568b1307ff3d07326f3b7c467c7006
Shannon Adams <77690999+shanfrom90@users.noreply.github.com>
2024-02-13 17:53:01 -0500

    attempting to connec to db, wip

--- src/main/java/com/battle/chat/demo/service/MessageService.java
+++ src/main/java/com/battle/chat/demo/service/MessageService.java
@@ -1,16 +1,18 @@
+package com.battle.chat.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.battle.chat.demo.model.Message;
  
 import com.battle.chat.demo.repository.MessageRepository;
-package com.battle.chat.demo.service;
+
 
 @Service
 public class MessageService {
     @Autowired
     MessageRepository messageRepository;
 
-     public void saveMessage(Message message){
+ 
+    public void saveMessage(Message message){
         messageRepository.save(message);
      }
 }
